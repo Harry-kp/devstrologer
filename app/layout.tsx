2d9ebfe24536d861cbc13309fa9eecd5327e61f0
Harshit Chaudhary <chaudharyharshit9@gmail.com>
2024-08-31 20:53:47 +0530

    Updated Link Preview for twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Head from "next/head";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -16,6 +18,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:title" content="DevStrologer" />
+        <meta property="og:description" content="Horoscope for developers" />
+        <meta
+          property="og:image"
+          content="https://github.com/user-attachments/assets/65400e98-e500-46c6-8cc5-17d881f7a6cb"
+        />
+        <meta property="og:url" content="https://devstrologer.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="DevStrologer" />
+        <meta
+          name="twitter:description"
+          content="DevStrologer - Horoscope for developers"
+        />
+        <meta
+          name="twitter:image"
+          content="https://github.com/user-attachments/assets/65400e98-e500-46c6-8cc5-17d881f7a6cb"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
